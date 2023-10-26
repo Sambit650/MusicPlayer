37dfc2c31548e19f0314f5559d0a710a18520992
Sambit Das <sambit650@gmail.com>
2023-10-26 20:09:57 +0530

    Initial Commit

---
+++ MusicPlayer/MusicPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicPlayerApp.swift
+//  MusicPlayer
+//
+//  Created by Sambit Das on 26/10/23.
+//
+
+import SwiftUI
+
+@main
+struct MusicPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
